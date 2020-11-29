59ebaed7ddaaa825e25084460db2713eb4c5f7c5
Jamin Ewell <jamin.ewell@yahoo.com>
2020-11-29 00:12:12 -0800

    Update index.js
    added product data

--- assignment12/src/index.js
+++ assignment12/src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+let PRODUCTS = {
+  '1': {id: 1, category: 'Outside', price: '$59.99', name: 'Rake'},
+  '2': {id: 2, category: 'Outside', price: '$3,000', name: 'Leaf Blower'},
+  '3': {id: 3, category: 'Outside', price: '$5.00', name: 'Hose'},
+  '4': {id: 4, category: 'Kitchen', price: '$49.99', name: 'Cutting Board'},
+  '5': {id: 5, category: 'Kitchen', price: '$15.99', name: 'Knife'},
+  '6': {id: 6, category: 'Kitchen', price: '$10', name: 'Bagel Cutter'}
+};
+
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
